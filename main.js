9e53e384a5306061c935a49d53b76cf43ff0a8fc
ericaraujogil <ericaraujogil@gmail.com>
2019-09-03 12:44:27 +0200

    reduced and loops

--- main.js
+++ main.js
@@ -1,283 +1,76 @@
 var checkclick = false;
-        var hearts = document.getElementsByClassName("corazon")
-
-        function Puntuacion(punt) {
-            event.stopPropagation();
-            console.log(punt.id)
-            if (punt.id == "pun1" && checkclick == false) {
-                punt.classList.add("corazonnegro")
-                punt.classList.remove("corazonrojo")
-            } else if (punt.id == "pun2" && checkclick == false) {
-                punt.classList.add("corazonnegro")
-                punt.classList.remove("corazonrojo")
-                document.getElementById("pun1").classList.add("corazonnegro")
-                document.getElementById("pun1").classList.remove("corazonrojo")
-            } else if (punt.id == "pun3" && checkclick == false) {
-                punt.classList.add("corazonnegro")
-                document.getElementById("pun1").classList.add("corazonnegro")
-                document.getElementById("pun1").classList.remove("corazonrojo")
-                document.getElementById("pun2").classList.add("corazonnegro")
-                document.getElementById("pun2").classList.remove("corazonrojo")
-            } else if (punt.id == "pun4" && checkclick == false) {
-                punt.classList.add("corazonnegro")
-                punt.classList.remove("corazonrojo")
-                document.getElementById("pun1").classList.add("corazonnegro")
-                document.getElementById("pun1").classList.remove("corazonrojo")
-                document.getElementById("pun2").classList.add("corazonnegro")
-                document.getElementById("pun2").classList.remove("corazonrojo")
-                document.getElementById("pun3").classList.add("corazonnegro")
-                document.getElementById("pun3").classList.remove("corazonrojo")
-            } else if (punt.id == "pun5" && checkclick == false) {
-                punt.classList.add("corazonnegro")
-                punt.classList.remove("corazonrojo")
-                document.getElementById("pun1").classList.add("corazonnegro")
-                document.getElementById("pun1").classList.remove("corazonrojo")
-                document.getElementById("pun2").classList.add("corazonnegro")
-                document.getElementById("pun2").classList.remove("corazonrojo")
-                document.getElementById("pun3").classList.add("corazonnegro")
-                document.getElementById("pun3").classList.remove("corazonrojo")
-                document.getElementById("pun4").classList.add("corazonnegro")
-                document.getElementById("pun4").classList.remove("corazonrojo")
-            }
+var hearts = document.getElementsByClassName("corazon")
+function Puntuacion(punt, clase) {
+    event.stopPropagation();
+    var comp = true;
+    Array.from(hearts).forEach((heart) => {
+        if (punt.id != heart.id && comp == true) {
+            heart.classList.add("corazonnegro")
+            heart.classList.remove("corazonrojo")
+        } else {
+            punt.classList.add("corazonnegro")
+            punt.classList.remove("corazonrojo")
+            comp = false
         }
+    })
+}
 
-        function qPuntuacion(punt) {
-            event.stopPropagation();
-            if (punt.id == "pun1" && checkclick == false) {
-                punt.classList.add("corazonrojo")
-                punt.classList.remove("corazonnegro")
-            } else if (punt.id == "pun2" && checkclick == false) {
-                punt.classList.add("corazonrojo")
-                punt.classList.remove("corazonnegro")
-                document.getElementById("pun1").classList.add("corazonrojo")
-                document.getElementById("pun1").classList.remove("corazonnegro")
-            } else if (punt.id == "pun3" && checkclick == false) {
-                punt.classList.add("corazonrojo")
-                punt.classList.remove("corazonnegro")
-                document.getElementById("pun1").classList.add("corazonrojo")
-                document.getElementById("pun1").classList.remove("corazonnegro")
-                document.getElementById("pun2").classList.add("corazonrojo")
-                document.getElementById("pun2").classList.remove("corazonnegro")
-            } else if (punt.id == "pun4" && checkclick == false) {
-                punt.classList.add("corazonrojo")
-                punt.classList.remove("corazonnegro")
-                document.getElementById("pun1").classList.add("corazonrojo")
-                document.getElementById("pun1").classList.remove("corazonnegro")
-                document.getElementById("pun2").classList.add("corazonrojo")
-                document.getElementById("pun2").classList.remove("corazonnegro")
-                document.getElementById("pun3").classList.add("corazonrojo")
-                document.getElementById("pun3").classList.remove("corazonnegro")
-            } else if (punt.id == "pun5" && checkclick == false) {
-                punt.classList.add("corazonrojo")
-                punt.classList.remove("corazonnegro")
-                document.getElementById("pun1").classList.add("corazonrojo")
-                document.getElementById("pun1").classList.remove("corazonnegro")
-                document.getElementById("pun2").classList.add("corazonrojo")
-                document.getElementById("pun2").classList.remove("corazonnegro")
-                document.getElementById("pun3").classList.add("corazonrojo")
-                document.getElementById("pun3").classList.remove("corazonnegro")
-                document.getElementById("pun4").classList.add("corazonrojo")
-                document.getElementById("pun4").classList.remove("corazonnegro")
-            }
+function qPuntuacion(punt) {
+    event.stopPropagation();
+    let comp = true;
+    Array.from(hearts).forEach((heart) => {
+        if (punt.id != heart.id && comp == true) {
+            heart.classList.add("corazonrojo")
+            heart.classList.remove("corazonnegro")
+        } else {
+            punt.classList.add("corazonrojo")
+            punt.classList.remove("corazonnegro")
+            comp = false
         }
+    })
+}
 
-        function sPuntuacion(punt) {
-            event.stopPropagation();
-            if (punt.classList.contains("selected")) {
-                if (punt.id == "pun1") {
-                    console.log("click1")
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id != "pun1") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                    })
-                    checkclick = false
-                } else if (punt.id == "pun2") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id != "pun1" && heart.id != "pun2") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        /*if (heart.id != "pun2") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }*/
-                    })
-                    checkclick = false
-                } else if (punt.id == "pun3") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id != "pun1" && heart.id != "pun2" && heart.id != "pun3") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        /*if (heart.id != "pun2") {
-                            heart.classList.remove("selected")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id != "pun3") {
-                            heart.classList.remove("selected")
-                            heart.classList.add("corazonrojo")
-                        }*/
-                    })
-                    checkclick = false
-                } else if (punt.id == "pun4") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id != "pun1" && heart.id != "pun2" && heart.id != "pun3" && heart.id != "pun4") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        /*if (heart.id == "pun2") {
-                            heart.classList.remove("selected")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id == "pun3") {
-                            heart.classList.remove("selected")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id == "pun4") {
-                            heart.classList.remove("selected")
-                            heart.classList.add("corazonrojo")
-                        }else{
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }*/
-                    })
-                    checkclick = false
-                } else if (punt.id == "pun5") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id == "pun1") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id == "pun2") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id == "pun3") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id == "pun4") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                        if (heart.id == "pun5") {
-                            heart.classList.remove("selected")
-                            heart.classList.remove("corazonnegro")
-                            heart.classList.add("corazonrojo")
-                        }
-                    })
-                    checkclick = false
-                }
+function sPuntuacion(punt) {
+    event.stopPropagation();
+    let comp = true;
+    if (punt.classList.contains("selected")) {
+        Array.from(hearts).forEach((heart) => {
+            if (comp == false) {
+                heart.classList.remove("selected")
+                heart.classList.remove("corazonnegro")
+                heart.classList.add("corazonrojo")
+                comp = false
+            }else if (punt.id == heart.id) {
+                comp = false
+            }
+        })
+        checkclick = true
+    } else {
+
+        Array.from(hearts).forEach((heart) => {
+            if (punt.id != heart.id && comp == true) {
+                heart.classList.remove("corazonrojo")
+                heart.classList.add("selected")
             } else {
-                if (punt.id == "pun1") {
-                    console.log("click1")
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id == "pun1") {
-                            console.log("cambiando1")
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                    })
-                    checkclick = true
-                } else if (punt.id == "pun2") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id == "pun1") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun2") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                    })
-                    checkclick = true
-                } else if (punt.id == "pun3") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id == "pun1") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun2") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun3") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                    })
-                    checkclick = true
-                } else if (punt.id == "pun4") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id == "pun1") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun2") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun3") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun4") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                    })
-                    checkclick = true
-                } else if (punt.id == "pun5") {
-                    Array.from(hearts).forEach((heart) => {
-                        if (heart.id == "pun1") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun2") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun3") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun4") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                        if (heart.id == "pun5") {
-                            heart.classList.remove("corazonrojo")
-                            heart.classList.add("selected")
-                        }
-                    })
-                    checkclick = true
-                }
+                punt.classList.remove("corazonrojo")
+                punt.classList.add("selected")
+                comp = false
             }
+        })
+        checkclick = true
+    }
+}
 
-        }
-
-        document.getElementById("container").onclick = function() {
-            event.stopPropagation();
-            if (checkclick) {
-                Array.from(hearts).forEach((heart) => {
-                    if (heart.classList.contains("selected")) {
-                        heart.classList.remove("selected")
-                        heart.classList.remove("corazonnegro")
-                        heart.classList.add("corazonrojo")
-                    }
-                })
-                checkclick = false
+document.getElementById("container").onclick = function () {
+    event.stopPropagation();
+    if (checkclick) {
+        Array.from(hearts).forEach((heart) => {
+            if (heart.classList.contains("selected")) {
+                heart.classList.remove("selected")
+                heart.classList.remove("corazonnegro")
+                heart.classList.add("corazonrojo")
             }
-        }
+        })
+        checkclick = false
+    }
+}
